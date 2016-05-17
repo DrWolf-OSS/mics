a8c362849fac9940e6d794d904cb7de41cd6cdaf
Stefano Raffini <stefanoraffini@drwolf.it>
2016-05-17 14:56:49 +0200

    Salvataggio Output e visualizzazione

--- MICS-ejb/ejbModule/it/drwolf/mics/session/home/SimulazioneHome.java
+++ MICS-ejb/ejbModule/it/drwolf/mics/session/home/SimulazioneHome.java
@@ -1,11 +1,23 @@
 package it.drwolf.mics.session.home;
 
-import it.drwolf.mics.entity.*;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityHome;
+
+import it.drwolf.mics.entity.Azienda;
+import it.drwolf.mics.entity.DatiBilancio;
+import it.drwolf.mics.entity.DomandaMercato;
+import it.drwolf.mics.entity.OutputSimulazione;
+import it.drwolf.mics.entity.OutputSimulazioneId;
+import it.drwolf.mics.entity.Simulazione;
+import it.drwolf.mics.entity.TrimestreSimulazione;
 
 @Name("simulazioneHome")
 public class SimulazioneHome extends EntityHome<Simulazione> {
@@ -15,13 +27,11 @@ public class SimulazioneHome extends EntityHome<Simulazione> {
 	@In(create = true)
 	DomandaMercatoHome domandaMercatoHome;
 
-	public void setSimulazioneId(Integer id) {
-		setId(id);
-	}
+	private ArrayList<OutputSimulazione> risultati = new ArrayList<OutputSimulazione>();
 
-	public Integer getSimulazioneId() {
-		return (Integer) getId();
-	}
+	private HashMap<String, ArrayList<ArrayList<Integer>>> visualized = new HashMap<String, ArrayList<ArrayList<Integer>>>();
+
+	private HashMap<String, String> labels = new HashMap<String, String>();
 
 	@Override
 	protected Simulazione createInstance() {
@@ -29,39 +39,144 @@ public class SimulazioneHome extends EntityHome<Simulazione> {
 		return simulazione;
 	}
 
-	public void load() {
-		if (isIdDefined()) {
-			wire();
-		}
-	}
-
-	public void wire() {
-		getInstance();
-		Azienda azienda = aziendaHome.getDefinedInstance();
-		if (azienda != null) {
-			getInstance().setAzienda(azienda);
-		}
-		DomandaMercato domandaMercato = domandaMercatoHome.getDefinedInstance();
-		if (domandaMercato != null) {
-			getInstance().setDomandaMercato(domandaMercato);
-		}
-	}
-
-	public boolean isWired() {
-		if (getInstance().getAzienda() == null)
-			return false;
-		if (getInstance().getDomandaMercato() == null)
-			return false;
-		return true;
+	public List<DatiBilancio> getDatiBilanci() {
+		return this.getInstance() == null ? null : new ArrayList<DatiBilancio>(this.getInstance().getDatiBilanci());
 	}
 
 	public Simulazione getDefinedInstance() {
-		return isIdDefined() ? getInstance() : null;
+		return this.isIdDefined() ? this.getInstance() : null;
 	}
 
-	public List<DatiBilancio> getDatiBilanci() {
-		return getInstance() == null ? null : new ArrayList<DatiBilancio>(
-				getInstance().getDatiBilanci());
+	public ArrayList<Entry<String, ArrayList<ArrayList<Integer>>>> getEntries() {
+		return new ArrayList<Entry<String, ArrayList<ArrayList<Integer>>>>(this.visualized.entrySet());
+	}
+
+	public HashMap<String, String> getLabels() {
+		return this.labels;
+	}
+
+	public ArrayList<OutputSimulazione> getRisultati() {
+
+		return this.risultati;
+	}
+
+	public ArrayList<ArrayList<Integer>> getRisultatiByName(String name) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		for (int index = 3; index < this.risultati.size(); index++) {
+			OutputSimulazione os = this.risultati.get(index);
+			try {
+				ArrayList<Integer> trimestre = new ArrayList<Integer>();
+				trimestre.add(os.getTrimestreSimulazione().getTrimestre());
+				Object res = os.getClass().getMethod(name, null).invoke(os, null);
+				if (res instanceof Double) {
+					Double d = (Double) res;
+					trimestre.add(d.intValue());
+				} else if (res instanceof BigDecimal) {
+					BigDecimal bd = (BigDecimal) res;
+					trimestre.add(bd.intValue());
+				} else if (res instanceof Integer) {
+					Integer i = (Integer) res;
+					trimestre.add(i);
+				} else {
+					trimestre.add(0);
+				}
+				result.add(trimestre);
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public Integer getSimulazioneId() {
+		this.loadRisultati();
+		return (Integer) this.getId();
+	}
+
+	public HashMap<String, ArrayList<ArrayList<Integer>>> getVisualized() {
+		return this.visualized;
+	}
+
+	public boolean isWired() {
+		if (this.getInstance().getAzienda() == null) {
+			return false;
+		}
+		if (this.getInstance().getDomandaMercato() == null) {
+			return false;
+		}
+		return true;
+	}
+
+	public void load() {
+		if (this.isIdDefined()) {
+			this.wire();
+		}
+	}
+
+	private void loadRisultati() {
+		List<TrimestreSimulazione> trimestri = this.getEntityManager().createQuery("from TrimestreSimulazione")
+				.getResultList();
+		for (TrimestreSimulazione trimestreSimulazione : trimestri) {
+			OutputSimulazioneId id = new OutputSimulazioneId(this.getInstance().getId(),
+					trimestreSimulazione.getTrimestre());
+			OutputSimulazione o = this.getEntityManager().find(OutputSimulazione.class, id);
+			this.risultati.add(o);
+
+		}
+
+		this.labels.put("Incremento da Innovazione", "getIncrementoDaInnovazione");
+		this.labels.put("Incremento da Reputazione", "getIncrementoDareputazione");
+		this.labels.put("Producivity", "getProductivity");
+		this.labels.put("Quota volumi aziendali", "getQuotaVolumiAziendali");
+		this.labels.put("Volumi di Mercato", "getVolumiMercato");
+		this.labels.put("Domanda Lavoro", "getDomandaLavoro");
+		this.labels.put("Esuberi", "getEsuberi");
+		this.labels.put("Lavoratori in Forza", "getLavoratoriInForza");
+		for (String label : this.labels.keySet()) {
+			this.visualized.put(label, this.getRisultatiByName(this.labels.get(label)));
+
+		}
+	}
+
+	public void setLabels(HashMap<String, String> labels) {
+		this.labels = labels;
+	}
+
+	public void setRisultati(ArrayList<OutputSimulazione> risultati) {
+		this.risultati = risultati;
+	}
+
+	public void setSimulazioneId(Integer id) {
+		this.setId(id);
+	}
+
+	public void setVisualized(HashMap<String, ArrayList<ArrayList<Integer>>> visualized) {
+		this.visualized = visualized;
+	}
+
+	public void wire() {
+		this.getInstance();
+		Azienda azienda = this.aziendaHome.getDefinedInstance();
+		if (azienda != null) {
+			this.getInstance().setAzienda(azienda);
+		}
+		DomandaMercato domandaMercato = this.domandaMercatoHome.getDefinedInstance();
+		if (domandaMercato != null) {
+			this.getInstance().setDomandaMercato(domandaMercato);
+		}
 	}
 
 }
--- MICS-ejb/ejbModule/it/drwolf/mics/util/SimulationBean.java
+++ MICS-ejb/ejbModule/it/drwolf/mics/util/SimulationBean.java
@@ -1,17 +1,5 @@
 package it.drwolf.mics.util;
 
-import it.drwolf.mics.entity.AnnoBilancio;
-import it.drwolf.mics.entity.Azienda;
-import it.drwolf.mics.entity.DatiBilancio;
-import it.drwolf.mics.entity.DatiBilancioId;
-import it.drwolf.mics.entity.DomandaMercato;
-import it.drwolf.mics.entity.Glossario;
-import it.drwolf.mics.entity.OutputSimulazione;
-import it.drwolf.mics.entity.Simulazione;
-import it.drwolf.mics.entity.TrimestreSimulazione;
-import it.drwolf.mics.model.MiICSiThinkModel;
-import it.drwolf.mics.session.home.DatiBilancioHome;
-
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -30,6 +18,19 @@ import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+
+import it.drwolf.mics.entity.AnnoBilancio;
+import it.drwolf.mics.entity.Azienda;
+import it.drwolf.mics.entity.DatiBilancio;
+import it.drwolf.mics.entity.DatiBilancioId;
+import it.drwolf.mics.entity.DomandaMercato;
+import it.drwolf.mics.entity.Glossario;
+import it.drwolf.mics.entity.OutputSimulazione;
+import it.drwolf.mics.entity.OutputSimulazioneId;
+import it.drwolf.mics.entity.Simulazione;
+import it.drwolf.mics.entity.TrimestreSimulazione;
+import it.drwolf.mics.model.MiICSiThinkModel;
+import it.drwolf.mics.session.home.DatiBilancioHome;
 
 @Name("simulationBean")
 @Scope(ScopeType.CONVERSATION)
@@ -79,42 +80,32 @@ public class SimulationBean {
 		this.getAlertMessages()
 				.add("Attenzione! Dall'analisi dei dati di bilanio inseriti sono emersi i seguenti problemi:");
 		if (this.datiBilancio.get(this.anno0).getRoe() != null
-				&& this.datiBilancio.get(this.anno0)
-						.getRendimentoInvestimentiSicuri() != null) {
-			if (this.datiBilancio
-					.get(this.anno0)
-					.getRoe()
-					.compareTo(
-							this.getDatiBilancio(this.anno0)
-									.getRendimentoInvestimentiSicuri()) <= 0) {
+				&& this.datiBilancio.get(this.anno0).getRendimentoInvestimentiSicuri() != null) {
+			if (this.datiBilancio.get(this.anno0).getRoe()
+					.compareTo(this.getDatiBilancio(this.anno0).getRendimentoInvestimentiSicuri()) <= 0) {
 				result = true;
-				this.getAlertMessages()
-						.add("Il valore del ROE non dobvrebbe essere uguale o inferiore al valore del rendimento in investimenti sicuri");
+				this.getAlertMessages().add(
+						"Il valore del ROE non dobvrebbe essere uguale o inferiore al valore del rendimento in investimenti sicuri");
 
 			}
 		}
 		if (this.datiBilancio.get(this.anno0).getEbitda() != null) {
-			if (this.datiBilancio.get(this.anno0).getEbitda()
-					.compareTo(new BigDecimal(0)) < 0) {
+			if (this.datiBilancio.get(this.anno0).getEbitda().compareTo(new BigDecimal(0)) < 0) {
 				result = true;
-				this.getAlertMessages().add(
-						"Il valore del EBITDA non dobvrebbe essere negativo");
+				this.getAlertMessages().add("Il valore del EBITDA non dobvrebbe essere negativo");
 			}
 		}
 		if (this.datiBilancio.get(this.anno0).getDebitiBancheSuFatturato() != null) {
-			if (this.datiBilancio.get(this.anno0).getDebitiBancheSuFatturato()
-					.compareTo(new BigDecimal(50)) > 0) {
+			if (this.datiBilancio.get(this.anno0).getDebitiBancheSuFatturato().compareTo(new BigDecimal(50)) > 0) {
 				result = true;
 				this.getAlertMessages()
 						.add("Il valore dei  Debiti v/banche su fatturato non dobvrebbe essere superiore al 50%");
 			}
 		}
 		if (this.datiBilancio.get(this.anno0).getRoi() != null) {
-			if (this.datiBilancio.get(this.anno0).getRoi()
-					.compareTo(new BigDecimal(0)) < 0) {
+			if (this.datiBilancio.get(this.anno0).getRoi().compareTo(new BigDecimal(0)) < 0) {
 				result = true;
-				this.getAlertMessages().add(
-						"Il valore del ROI non dobvrebbe essere negativo");
+				this.getAlertMessages().add("Il valore del ROI non dobvrebbe essere negativo");
 			}
 		}
 		return result;
@@ -150,10 +141,8 @@ public class SimulationBean {
 
 		// a questo punto devo salvare i Dati di Bilancio
 		for (Integer anno : this.datiBilancio.keySet()) {
-			AnnoBilancio annoBilancio = this.entityManager.find(
-					AnnoBilancio.class, anno);
-			DatiBilancioId datiBilancioId = new DatiBilancioId(
-					simulazione.getId(), anno);
+			AnnoBilancio annoBilancio = this.entityManager.find(AnnoBilancio.class, anno);
+			DatiBilancioId datiBilancioId = new DatiBilancioId(simulazione.getId(), anno);
 			DatiBilancio datiBilancio = this.datiBilancio.get(anno);
 			datiBilancio.setAnnoBilancio(annoBilancio);
 			datiBilancio.setSimulazione(simulazione);
@@ -238,8 +227,7 @@ public class SimulationBean {
 	}
 
 	public ArrayList<Entry<String, ArrayList<ArrayList<Integer>>>> getEntries() {
-		return new ArrayList<Entry<String, ArrayList<ArrayList<Integer>>>>(
-				this.visualized.entrySet());
+		return new ArrayList<Entry<String, ArrayList<ArrayList<Integer>>>>(this.visualized.entrySet());
 	}
 
 	public Double getInnovazioneDiProdotto() {
@@ -255,8 +243,7 @@ public class SimulationBean {
 	}
 
 	public List<Simulazione> getListaSimulazioni() {
-		return this.entityManager.createQuery("from Simulazione")
-				.getResultList();
+		return this.entityManager.createQuery("from Simulazione order by dataInserimento desc").getResultList();
 	}
 
 	public Integer getPercentualeCostiProduzioneTerritorio() {
@@ -288,8 +275,7 @@ public class SimulationBean {
 			try {
 				ArrayList<Integer> trimestre = new ArrayList<Integer>();
 				trimestre.add(os.getTrimestreSimulazione().getTrimestre());
-				Object res = os.getClass().getMethod(name, null)
-						.invoke(os, null);
+				Object res = os.getClass().getMethod(name, null).invoke(os, null);
 				if (res instanceof Double) {
 					Double d = (Double) res;
 					trimestre.add(d.intValue());
@@ -360,8 +346,7 @@ public class SimulationBean {
 	public String getUnitaDiMisur(String name) {
 		String unita = "";
 		try {
-			Glossario voce = (Glossario) this.entityManager
-					.createQuery("from Glossario where name= :name")
+			Glossario voce = (Glossario) this.entityManager.createQuery("from Glossario where name= :name")
 					.setParameter("name", name).getSingleResult();
 			unita = voce.getUnitaDiMisura();
 		} catch (NoResultException e) {
@@ -377,8 +362,7 @@ public class SimulationBean {
 	public String getVocabolo(String name) {
 		String vocabolo = "";
 		try {
-			Glossario voce = (Glossario) this.entityManager
-					.createQuery("from Glossario where name = :name")
+			Glossario voce = (Glossario) this.entityManager.createQuery("from Glossario where name = :name")
 					.setParameter("name", name).getSingleResult();
 			vocabolo = voce.getVocabolo();
 		} catch (NoResultException e) {
@@ -390,8 +374,7 @@ public class SimulationBean {
 	public String getVocaboloDef(String name) {
 		String definizione = "";
 		try {
-			Glossario voce = (Glossario) this.entityManager
-					.createQuery("from Glossario where name = :name")
+			Glossario voce = (Glossario) this.entityManager.createQuery("from Glossario where name = :name")
 					.setParameter("name", name).getSingleResult();
 			definizione = voce.getDefinizione();
 		} catch (NoResultException e) {
@@ -441,13 +424,11 @@ public class SimulationBean {
 		this.innovazioneProdottoSpinner = innovazioneProdottoSpinner;
 	}
 
-	public void setPercentualeCostiProduzioneTerritorio(
-			Integer percentualeCostiProduzioneTerritorio) {
+	public void setPercentualeCostiProduzioneTerritorio(Integer percentualeCostiProduzioneTerritorio) {
 		this.percentualeCostiProduzioneTerritorio = percentualeCostiProduzioneTerritorio;
 	}
 
-	public void setPercentualeIndottoCongiunturaleTerritorio(
-			Integer percentualeIndottoCongiunturaleTerritorio) {
+	public void setPercentualeIndottoCongiunturaleTerritorio(Integer percentualeIndottoCongiunturaleTerritorio) {
 		this.percentualeIndottoCongiunturaleTerritorio = percentualeIndottoCongiunturaleTerritorio;
 	}
 
@@ -467,8 +448,7 @@ public class SimulationBean {
 		this.solutionType = solutionType;
 	}
 
-	public void setVisualized(
-			HashMap<String, ArrayList<ArrayList<Integer>>> visualized) {
+	public void setVisualized(HashMap<String, ArrayList<ArrayList<Integer>>> visualized) {
 		this.visualized = visualized;
 	}
 
@@ -476,15 +456,11 @@ public class SimulationBean {
 		System.out.println("Avvio la simulazione");
 		System.out.println(this.datiBilancioHome.getInstance().getId());
 
-		MiICSiThinkModel micsModel = new MiICSiThinkModel(this.entityManager,
-				this, new Double(
-						this.getPercentualeIndottoCongiunturaleTerritorio()));
-		this.risultati = micsModel.execute(this.datiBilancioHome.getInstance(),
-				this.domandaMercato);
-		this.labels.put("Incremento da Innovazione",
-				"getIncrementoDaInnovazione");
-		this.labels.put("Incremento da Reputazione",
-				"getIncrementoDareputazione");
+		MiICSiThinkModel micsModel = new MiICSiThinkModel(this.entityManager, this,
+				new Double(this.getPercentualeIndottoCongiunturaleTerritorio()));
+		this.risultati = micsModel.execute(this.datiBilancioHome.getInstance(), this.domandaMercato);
+		this.labels.put("Incremento da Innovazione", "getIncrementoDaInnovazione");
+		this.labels.put("Incremento da Reputazione", "getIncrementoDareputazione");
 		this.labels.put("Producivity", "getProductivity");
 		this.labels.put("Quota volumi aziendali", "getQuotaVolumiAziendali");
 		this.labels.put("Volumi di Mercato", "getVolumiMercato");
@@ -492,10 +468,17 @@ public class SimulationBean {
 		this.labels.put("Esuberi", "getEsuberi");
 		this.labels.put("Lavoratori in Forza", "getLavoratoriInForza");
 		for (String label : this.labels.keySet()) {
-			this.visualized.put(label,
-					this.getRisultatiByName(this.labels.get(label)));
+			this.visualized.put(label, this.getRisultatiByName(this.labels.get(label)));
 
 		}
 
+		for (OutputSimulazione os : this.risultati) {
+			OutputSimulazioneId id = new OutputSimulazioneId(
+					this.datiBilancioHome.getInstance().getSimulazione().getId(),
+					os.getTrimestreSimulazione().getTrimestre());
+			os.setId(id);
+			os.setSimulazione(this.datiBilancioHome.getInstance().getSimulazione());
+			this.entityManager.persist(os);
+		}
 	}
 }
